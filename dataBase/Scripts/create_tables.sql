3a2e2de8613d8a6fd153add753ff53d0c6072054
Podlesnykh Dmitry <buffol.dp@gmail.com>
2016-01-23 01:34:00 +0300

    update db scripts

--- dataBase/Scripts/create_tables.sql
+++ dataBase/Scripts/create_tables.sql
@@ -1,21 +1,22 @@
 
-CREATE TABLE Pj_Attributes
-(
-	Attribute_ID         INTEGER NOT NULL ,
-	Attribute_Name       VARCHAR2(50) NOT NULL 
-);
+DROP TABLE Params CASCADE CONSTRAINTS PURGE;
 
-ALTER TABLE Pj_Attributes
-	ADD CONSTRAINT  XPKAttributes PRIMARY KEY (Attribute_ID);
+DROP TABLE Pj_Attributes CASCADE CONSTRAINTS PURGE;
 
-CREATE TABLE Fin_Object_Atr_types
+DROP TABLE Transactions CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE Fin_objects CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE Pj_Users CASCADE CONSTRAINTS PURGE;
+DROP TABLE Fin_Object_Types CASCADE CONSTRAINTS PURGE;
+
+CREATE TABLE Fin_Object_Types
 (
 	Fin_Object_type_ID   INTEGER NOT NULL ,
-	Fin_Object_type_name VARCHAR2(50) NOT NULL ,
-	Attribute_ID         INTEGER NOT NULL 
+	Fin_Object_type_name VARCHAR2(50) NOT NULL 
 );
 
-ALTER TABLE Fin_Object_Atr_types
+ALTER TABLE Fin_Object_Types
 	ADD CONSTRAINT  XPKFin_Object_types PRIMARY KEY (Fin_Object_type_ID);
 
 CREATE TABLE Fin_objects
@@ -30,31 +31,22 @@ CREATE TABLE Fin_objects
 ALTER TABLE Fin_objects
 	ADD CONSTRAINT  XPKFin_objects PRIMARY KEY (Fin_object_id);
 
+CREATE TABLE Pj_Attributes
+(
+	Attribute_Name       VARCHAR2(50) NOT NULL ,
+	Fin_Object_type_ID   INTEGER NOT NULL ,
+	Attribute_ID         INTEGER NOT NULL 
+);
+
+ALTER TABLE Pj_Attributes
+	ADD CONSTRAINT  XPKAttributes PRIMARY KEY (Attribute_ID);
+
 CREATE TABLE Params
 (
 	value1               VARCHAR2(4000) NULL ,
 	value_date           DATE NULL ,
-	Attribute_ID         INTEGER NOT NULL ,
-	Fin_object_id        INTEGER NOT NULL 
-);
-
-CREATE TABLE Pj_Users
-(
-	User_ID              INTEGER NOT NULL ,
-	Login                VARCHAR2(20) NOT NULL ,
-	Hash_sum             INTEGER NOT NULL ,
-	Name                 VARCHAR2(50) NULL ,
-	Account_Type         VARCHAR2(50) NOT NULL ,
-	Salt                 INTEGER NOT NULL 
-);
-
-ALTER TABLE Pj_Users
-	ADD CONSTRAINT  XPKUsers PRIMARY KEY (User_ID);
-
-CREATE TABLE User_resources
-(
-	User_ID              INTEGER NOT NULL ,
-	Fin_object_id        INTEGER NOT NULL 
+	Fin_object_id        INTEGER NOT NULL ,
+	Attribute_ID         INTEGER NOT NULL 
 );
 
 CREATE TABLE Transactions
@@ -69,11 +61,18 @@ CREATE TABLE Transactions
 ALTER TABLE Transactions
 	ADD CONSTRAINT  XPKTransactions PRIMARY KEY (Transaction_ID);
 
-ALTER TABLE Fin_Object_Atr_types
-	ADD (CONSTRAINT R_22 FOREIGN KEY (Attribute_ID) REFERENCES Pj_Attributes (Attribute_ID));
+CREATE TABLE Pj_Users
+(
+	User_ID              INTEGER NOT NULL ,
+	Login                VARCHAR2(20) NOT NULL ,
+	Hash_sum             INTEGER NOT NULL ,
+	Name                 VARCHAR2(50) NULL ,
+	Account_Type         VARCHAR2(50) NOT NULL ,
+	Salt                 INTEGER NOT NULL 
+);
 
-ALTER TABLE Fin_objects
-	ADD (CONSTRAINT R_18 FOREIGN KEY (Fin_Object_type_ID) REFERENCES Fin_Object_Atr_types (Fin_Object_type_ID));
+ALTER TABLE Pj_Users
+	ADD CONSTRAINT  XPKUsers PRIMARY KEY (User_ID);
 
 ALTER TABLE Fin_objects
 	ADD (CONSTRAINT R_20 FOREIGN KEY (Fin_object_id) REFERENCES Fin_objects (Fin_object_id) ON DELETE SET NULL);
@@ -81,20 +80,17 @@ ALTER TABLE Fin_objects
 ALTER TABLE Fin_objects
 	ADD (CONSTRAINT R_23 FOREIGN KEY (User_ID) REFERENCES Pj_Users (User_ID));
 
-ALTER TABLE Params
-	ADD (CONSTRAINT R_7 FOREIGN KEY (Attribute_ID) REFERENCES Pj_Attributes (Attribute_ID));
+ALTER TABLE Fin_objects
+	ADD (CONSTRAINT R_18 FOREIGN KEY (Fin_Object_type_ID) REFERENCES Fin_Object_Types (Fin_Object_type_ID));
+
+ALTER TABLE Pj_Attributes
+	ADD (CONSTRAINT R_25 FOREIGN KEY (Fin_Object_type_ID) REFERENCES Fin_Object_Types (Fin_Object_type_ID));
 
 ALTER TABLE Params
 	ADD (CONSTRAINT R_8 FOREIGN KEY (Fin_object_id) REFERENCES Fin_objects (Fin_object_id));
 
-ALTER TABLE User_resources
-	ADD (CONSTRAINT R_9 FOREIGN KEY (User_ID) REFERENCES Pj_Users (User_ID));
-
-ALTER TABLE User_resources
-	ADD (CONSTRAINT R_10 FOREIGN KEY (Fin_object_id) REFERENCES Fin_objects (Fin_object_id));
+ALTER TABLE Params
+	ADD (CONSTRAINT R_26 FOREIGN KEY (Attribute_ID) REFERENCES Pj_Attributes (Attribute_ID));
 
 ALTER TABLE Transactions
 	ADD (CONSTRAINT R_16 FOREIGN KEY (Fin_object_id) REFERENCES Fin_objects (Fin_object_id));
-
-ALTER TABLE Transactions
-	ADD (CONSTRAINT R_21 FOREIGN KEY (User_ID) REFERENCES Pj_Users (User_ID));
