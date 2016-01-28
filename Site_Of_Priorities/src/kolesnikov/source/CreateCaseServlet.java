82728e9d4fd6d6be64f746f596e7d7ad9fb41094
Artyom Kolesnikov <gggoogllle@gmail.com>
2016-01-28 20:41:12 +0300

    start create case

---
+++ Site_Of_Priorities/src/kolesnikov/source/CreateCaseServlet.java
@@ -0,0 +1,28 @@
+package kolesnikov.source;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/CreateCaseServlet")
+public class CreateCaseServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Cases caze = null;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+		caze.setCaseName(new String(request.getParameter("type_case").getBytes("ISO-8859-1"),"UTF-8"));
+		
+		
+		
+		
+		
+	}
+
+
+}
--- Site_Of_Priorities/src/kolesnikov/source/ObjectsController.java
+++ Site_Of_Priorities/src/kolesnikov/source/ObjectsController.java
@@ -11,18 +11,19 @@ public class ObjectsController {
 	private Connection connect = ConnectionFactory.getConnection();
 	private Statement statement = null;
 	private PreparedStatement prepareState = null;  // для шаблона запроса к БД
+	private String query = null; //запрос
+	private ResultSet result = null;
 	
 	// добавляет объект в базу данных
 	public boolean addObjectToDatabase(Object object) {
 		try {
 			statement = connect.createStatement();
 			prepareState = connect.prepareStatement("INSERT INTO Pj_Users(login, hash_sum, account_type, salt) VALUES ( ? , ? , ? , ? )");
+		
 			//если объект типа Users
 			if(object instanceof Users) {
 				Users user = (Users)object;
-				ResultSet result = null;
 				String login = user.getLogin();
-				String query = null; //запрос
 				query = "SELECT login FROM pj_users where login like '" + login +"'";
 				result = statement.executeQuery(query); // отправляем запрос
 				if(!result.next()) {
@@ -40,6 +41,45 @@ public class ObjectsController {
 					return true;
 				}
 			}
+			//если объект типа Cases
+			if(object instanceof Cases) {
+				Cases caze = (Cases)object;
+				int caseTypeId = 0;
+				int parentId = 0;
+				query = "SELECT fin_object_type_id FROM fin_object_types WHERE fin_object_type_name like '" + caze.getCaseType() + "'";
+				result = statement.executeQuery(query);
+				//проверяем если такой атрибут
+				if(!result.next()) {
+					query = "INSERT INTO fin_object_types(fin_object_type_names) VALUES('" + caze.getCaseType() + "')";
+					statement.executeUpdate(query);
+				}
+				statement.close();
+				//обновляем данные
+				result = statement.executeQuery(query);
+				result.next();
+				caseTypeId = result.getInt(1);
+				
+				// проверяем является ли это задача подзадачей другой
+				query = "SELECT fin_object_id FROM fin_objects WHERE object_name like '" + caze.getParentTypeName() + "'";
+				result = statement.executeQuery(query);
+				if(result.next()) {
+					parentId = result.getInt(1);
+				};
+		
+				//добавляем описание задачи в БД
+				prepareState = connect.prepareStatement("INSERT INTO fin_objects(parent_id, object_name, fin_object_type_id, user_id) VALUES( ? , ? , ? , ? )");
+				if(parentId != 0)
+					prepareState.setInt(1, parentId);
+				prepareState.setString(2, caze.getCaseName());
+				prepareState.setInt(3, caseTypeId);
+				prepareState.setInt(4, caze.getUserId());
+				
+
+			}
+			
+			
+			
+			
 		} 
 		catch (SQLException e) {
 			e.printStackTrace();
