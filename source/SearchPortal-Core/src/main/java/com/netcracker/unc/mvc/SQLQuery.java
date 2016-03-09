538a8bdd3d331f130dc85a65eb881bef78b016f0
Shumaherr <shumaherr@gmail.com>
2016-03-09 16:17:53 +0300

    Update SQLQuery.java

--- source/SearchPortal-Core/src/main/java/com/netcracker/unc/mvc/SQLQuery.java
+++ source/SearchPortal-Core/src/main/java/com/netcracker/unc/mvc/SQLQuery.java
@@ -189,6 +189,8 @@ public class SQLQuery {
 			+ invoice_id;
 	public static final String get_sum_all_balances_for_users_by_user_id = "select SUM(p.VALUE) from SP_FIN_OBJECTS o, SP_PARAMS p where p.FIN_OBJECT_ID=o.FIN_OBJECT_ID and USER_ID=? and p.ATTRIBUTE_ID=1 and o.FIN_OBJECT_TYPE_ID="
 			+ invoice_id;
+	public static final String get_sum_all_consumption_for_users_by_user_id = "SELECT SUM(par.VALUE) FROM SP_FIN_OBJECTS fo INNER JOIN SP_ATTRIBUTES atr ON fo.FIN_OBJECT_TYPE_ID = atr.FIN_OBJECT_TYPE_ID INNER JOIN SP_PARAMS par ON atr.ATTRIBUTE_ID = par.ATTRIBUTE_ID WHERE FIN_OBJECT_TYPE_ID = 3 AND date_format(par.VALUE_DATE, '%Y%m') = date_format(now(), '%Y%m')"
+			+ invoice_id;
 	public static final String get_invoices_and_balance_and_credit_and_percent = "select o.OBJECT_NAME, p.VALUE, a.VALUE, r.VALUE\n"
 			+ "from SP_FIN_OBJECTS o, SP_PARAMS p, SP_PARAMS a, SP_PARAMS r\n"
 			+ "where  p.FIN_OBJECT_ID=o.FIN_OBJECT_ID and a.FIN_OBJECT_ID=o.FIN_OBJECT_ID and r.FIN_OBJECT_ID=o.FIN_OBJECT_ID and o.USER_ID= ?\n"
