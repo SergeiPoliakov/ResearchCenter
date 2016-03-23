ff1792bd36b09c50686aebee101a185ce5faec8a
Alexandr Odintsov <shumaherr@gmail.com>
2016-03-24 01:09:21 +0300

    1 form remake

---
+++ source/SearchPortal-Web/src/main/webapp/javascripts/modulesHide.js
@@ -0,0 +1,15 @@
+function show_income(){
+    document.getElementById('income').style.display='block'; 
+    document.getElementById('consump').style.display = 'none';
+	document.getElementById('stat').style.display = 'none';
+}
+function show_consump(){
+    document.getElementById('income').style.display='none'; 
+    document.getElementById('consump').style.display = 'block';
+	document.getElementById('stat').style.display = 'none';
+}
+function show_income(){
+    document.getElementById('income').style.display='none'; 
+    document.getElementById('consump').style.display = 'none';
+	document.getElementById('stat').style.display = 'block';
+}
