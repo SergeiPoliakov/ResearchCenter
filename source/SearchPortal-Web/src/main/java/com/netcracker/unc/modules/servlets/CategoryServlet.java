cf2d7c5ba7fe3b149d2e08f1ab6ae5c8c5199528
Podlesnykh Dmitry <buffol.dp@gmail.com>
2016-03-23 08:52:26 +0300

    encoding

--- source/SearchPortal-Web/src/main/java/com/netcracker/unc/modules/servlets/CategoryServlet.java
+++ source/SearchPortal-Web/src/main/java/com/netcracker/unc/modules/servlets/CategoryServlet.java
@@ -41,6 +41,7 @@ public class CategoryServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
 
 		String action = null;
 		int objectId = 0;
