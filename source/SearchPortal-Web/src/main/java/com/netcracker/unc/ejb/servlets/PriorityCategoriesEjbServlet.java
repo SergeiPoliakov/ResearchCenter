b5e62b793a50a520a9274deab738f9e8dc691d6d
Podlesnykh Dmitry <buffol.dp@gmail.com>
2016-04-12 00:26:38 +0300

    fail query

--- source/SearchPortal-Web/src/main/java/com/netcracker/unc/ejb/servlets/PriorityCategoriesEjbServlet.java
+++ source/SearchPortal-Web/src/main/java/com/netcracker/unc/ejb/servlets/PriorityCategoriesEjbServlet.java
@@ -1,11 +1,20 @@
 package com.netcracker.unc.ejb.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.netcracker.unc.newmvc.ejb.controllers.ControllerCategories;
+import com.netcracker.unc.newmvc.ejb.entities.EntityUser;
+import com.netcracker.unc.newmvc.ejb.models.CategoryModel;
+import com.netcracker.unc.newmvc.ejb.models.UserModel;
 
 /**
  * Servlet implementation class PriorityCategoriesEjbServlet
@@ -13,14 +22,28 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/PriorityCategoriesEjbServlet")
 public class PriorityCategoriesEjbServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
+	@EJB
+	ControllerCategories controller;
+
+	@EJB
+	UserModel user;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
+		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		
+		user.setUser((EntityUser) request.getSession().getAttribute("user"));
+		
+		List categoryList = new ArrayList<CategoryModel>();
+		categoryList = controller.getPriorities(user.getUser());
+
+		request.getSession().setAttribute("categoryList", categoryList);
+	}
 }
